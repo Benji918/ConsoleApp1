07964182308babe429c65f89e224c2975fa55db0
Benjamin <kodiugos@gmail.com>
2024-08-11 22:03:56 -0500

    file changes

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Give me a number");
+string user_number = Console.ReadLine();
+int num1 = 0;
+
+
+if (int.TryParse(user_number, out num1))
+{
+    Console.WriteLine("You entered a number");
+}
+else
+{
+    Console.WriteLine("You didn\'t enter a number but a string bitvh");
+}
+
+Console.WriteLine($"User entered number + 1 {num1}");
+
+Console.ReadKey();
